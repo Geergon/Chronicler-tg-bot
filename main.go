2b9a52d11bcf345ac44b394e71da08a24af02ec6
Geergon <yura8068@gmail.com>
2026-06-14 16:42:19 +0300

    chore: go project initialization

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"github.com/tdewolff/canvas"
+)
+
+var (
+	notoSansFamily *canvas.FontFamily
+	notoMonoFamily *canvas.FontFamily
+	emojiFamily    *canvas.FontFamily
+)
+
+func main() {
+}
