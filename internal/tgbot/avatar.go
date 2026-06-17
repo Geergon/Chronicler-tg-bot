a71022319680891f8673a4f018ee85efd63fe332
Geergon <yura8068@gmail.com>
2026-06-17 10:56:06 +0300

    feat: add downloading user avatar

---
+++ internal/tgbot/avatar.go
@@ -0,0 +1,148 @@
+package tgbot
+
+import (
+	"bytes"
+	"fmt"
+	"image"
+	"log"
+
+	"github.com/celestix/gotgproto/ext"
+	"github.com/gotd/td/tg"
+)
+
+func fetchUserAvatar(ctx *ext.Context, userID int64) (image.Image, error) {
+	inputPeer := ctx.PeerStorage.GetInputPeerById(userID)
+	inputUser, ok := toInputUser(inputPeer)
+	if !ok {
+		return nil, fmt.Errorf("cannot resolve user %d", userID)
+	}
+
+	photos, err := ctx.Raw.PhotosGetUserPhotos(ctx, &tg.PhotosGetUserPhotosRequest{
+		UserID: inputUser,
+		Offset: 0,
+		Limit:  1,
+	})
+	if err != nil {
+		return nil, fmt.Errorf("PhotosGetUserPhotos: %w", err)
+	}
+
+	var photoList []tg.PhotoClass
+	switch p := photos.(type) {
+	case *tg.PhotosPhotos:
+		photoList = p.Photos
+	case *tg.PhotosPhotosSlice:
+		photoList = p.Photos
+	}
+
+	if len(photoList) == 0 {
+		log.Printf("fetchUserAvatar: no photos for user %d", userID)
+		return nil, nil
+	}
+
+	photo, ok := photoList[0].(*tg.Photo)
+	if !ok {
+		log.Printf("fetchUserAvatar: photo[0] is %T, not *tg.Photo", photoList[0])
+		return nil, nil
+	}
+
+	// log.Printf("fetchUserAvatar: photo ID=%d sizes=%d", photo.ID, len(photo.Sizes))
+
+	for _, s := range photo.Sizes {
+		log.Printf("  size type=%T %+v", s, s)
+	}
+
+	bestSize := pickBestPhotoSize(photo.Sizes)
+	if bestSize == nil {
+		log.Printf("fetchUserAvatar: no suitable size found")
+		return nil, nil
+	}
+	// log.Printf("fetchUserAvatar: bestSize type=%s", bestSize.Type)
+
+	location := &tg.InputPhotoFileLocation{
+		ID:            photo.ID,
+		AccessHash:    photo.AccessHash,
+		FileReference: photo.FileReference,
+		ThumbSize:     bestSize.Type,
+	}
+
+	img, err := downloadFile(ctx, location)
+	return img, err
+}
+
+func toInputUser(peer tg.InputPeerClass) (tg.InputUserClass, bool) {
+	switch p := peer.(type) {
+	case *tg.InputPeerUser:
+		return &tg.InputUser{
+			UserID:     p.UserID,
+			AccessHash: p.AccessHash,
+		}, true
+	case *tg.InputPeerSelf:
+		return &tg.InputUserSelf{}, true
+	}
+	return nil, false
+}
+
+func pickBestPhotoSize(sizes []tg.PhotoSizeClass) *tg.PhotoSize {
+	preferred := []string{"a", "b", "c"}
+	sizeMap := make(map[string]*tg.PhotoSize)
+	for _, s := range sizes {
+		switch ps := s.(type) {
+		case *tg.PhotoSize:
+			sizeMap[ps.Type] = ps
+		case *tg.PhotoSizeProgressive:
+			// конвертуємо в PhotoSize для сумісності
+			sizeMap[ps.Type] = &tg.PhotoSize{
+				Type: ps.Type,
+				W:    ps.W,
+				H:    ps.H,
+				Size: ps.Sizes[len(ps.Sizes)-1], // беремо найбільший варіант
+			}
+		}
+	}
+	for _, t := range preferred {
+		if s, ok := sizeMap[t]; ok {
+			return s
+		}
+	}
+	return nil
+}
+
+func downloadFile(ctx *ext.Context, location tg.InputFileLocationClass) (image.Image, error) {
+	var buf []byte
+	offset := 0
+	limit := 512 * 1024 // 512KB
+
+	for {
+		result, err := ctx.Raw.UploadGetFile(ctx, &tg.UploadGetFileRequest{
+			Location: location,
+			Offset:   int64(offset),
+			Limit:    limit,
+		})
+		if err != nil {
+			return nil, fmt.Errorf("UploadGetFile: %w", err)
+		}
+
+		file, ok := result.(*tg.UploadFile)
+		if !ok {
+			break
+		}
+
+		buf = append(buf, file.Bytes...)
+
+		if len(file.Bytes) < limit {
+			break
+		}
+		offset += len(file.Bytes)
+	}
+
+	if len(buf) == 0 {
+		return nil, nil
+	}
+
+	img, _, err := image.Decode(bytes.NewReader(buf))
+	if err != nil {
+		return nil, fmt.Errorf("image.Decode: %w", err)
+	}
+
+	return img, nil
+}
--- internal/tgbot/quote.go
+++ internal/tgbot/quote.go
@@ -147,11 +147,17 @@ func HandleQuote(ctx *ext.Context, update *ext.Update) error {
 			text = replyHeader.QuoteText
 		}
 
+		avatar, err := fetchUserAvatar(ctx, quoteData.Author.ID)
+		if err != nil {
+			log.Printf("fetchUserAvatar: %v", err)
+		}
+
 		messages := []render.ChatMessage{
 			{
 				AuthorID:    quoteData.Author.ID,
 				AuthorName:  quoteData.Author.FirstName,
 				Reply:       replyInfo,
+				AvatarImg:   avatar,
 				BubbleColor: color.RGBA{45, 40, 60, 255},
 				Segments: []render.TextSegment{
 					{Text: text, Color: color.RGBA{255, 255, 255, 255}},
@@ -237,9 +243,15 @@ func handleMessageStack(ctx *ext.Context, chatID int64, replyToMsgID int, number
 			text = replyHeader.QuoteText
 		}
 
+		avatar, err := fetchUserAvatar(ctx, quoteData.Author.ID)
+		if err != nil {
+			log.Printf("fetchUserAvatar: %v", err)
+		}
+
 		chatMessages = append(chatMessages, render.ChatMessage{
 			AuthorID:    quoteData.Author.ID,
 			AuthorName:  quoteData.Author.FirstName,
+			AvatarImg:   avatar,
 			Reply:       replyInfo,
 			BubbleColor: color.RGBA{45, 40, 60, 255},
 			Segments: []render.TextSegment{
@@ -249,142 +261,4 @@ func handleMessageStack(ctx *ext.Context, chatID int64, replyToMsgID int, number
 	}
 
 	return chatMessages, nil
-}
-
-func getHistory(ctx *ext.Context, chatID int64, msgID int, limit int) ([]*tg.Message, map[int64]*tg.User, error) {
-	if limit <= 0 || limit > 6 {
-		return nil, nil, fmt.Errorf("invalid limit: %d", limit)
-	}
-
-	fetchLimit := limit + 5
-	ids := make([]tg.InputMessageClass, fetchLimit)
-	for i := range ids {
-		ids[i] = &tg.InputMessageID{ID: msgID + i}
-	}
-
-	inputPeer := ctx.PeerStorage.GetInputPeerById(chatID)
-	var msgClass tg.MessagesMessagesClass
-	var err error
-
-	switch peer := inputPeer.(type) {
-	case *tg.InputPeerChannel:
-		msgClass, err = ctx.Raw.ChannelsGetMessages(ctx, &tg.ChannelsGetMessagesRequest{
-			Channel: &tg.InputChannel{
-				ChannelID:  peer.ChannelID,
-				AccessHash: peer.AccessHash,
-			},
-			ID: ids,
-		})
-	case *tg.InputPeerUser, *tg.InputPeerChat, *tg.InputPeerSelf:
-		msgClass, err = ctx.Raw.MessagesGetMessages(ctx, ids)
-	default:
-		return nil, nil, fmt.Errorf("unsupported peer type: %T", inputPeer)
-	}
-
-	if err != nil {
-		log.Printf("failed to get messages: %v", err)
-		return nil, nil, err
-	}
-	var rawMessages []tg.MessageClass
-	var users []tg.UserClass
-
-	switch r := msgClass.(type) {
-	case *tg.MessagesChannelMessages:
-		rawMessages = r.Messages
-		users = r.Users
-	case *tg.MessagesMessages:
-		rawMessages = r.Messages
-		users = r.Users
-	case *tg.MessagesMessagesSlice:
-		rawMessages = r.Messages
-		users = r.Users
-	default:
-		return nil, nil, fmt.Errorf("unknown messages response type: %T", msgClass)
-	}
-
-	userMap := make(map[int64]*tg.User)
-	for _, u := range users {
-		if user, ok := u.(*tg.User); ok {
-			userMap[user.ID] = user
-		}
-	}
-
-	var messages []*tg.Message
-	for _, m := range rawMessages {
-		msg, ok := m.(*tg.Message)
-		if !ok {
-			continue // MessageEmpty або MessageService
-		}
-		if msg.Message == "" && msg.Media == nil {
-			continue // empty
-		}
-		messages = append(messages, msg)
-		if len(messages) == limit {
-			break
-		}
-	}
-
-	return messages, userMap, nil
-}
-
-func fetchMessage(ctx *ext.Context, chatID int64, msgID int) (*tg.Message, map[int64]*tg.User, error) {
-	inputPeer := ctx.PeerStorage.GetInputPeerById(chatID)
-	var msgClass tg.MessagesMessagesClass
-	var err error
-
-	switch peer := inputPeer.(type) {
-	case *tg.InputPeerChannel:
-		msgClass, err = ctx.Raw.ChannelsGetMessages(ctx, &tg.ChannelsGetMessagesRequest{
-			Channel: &tg.InputChannel{
-				ChannelID:  peer.ChannelID,
-				AccessHash: peer.AccessHash,
-			},
-			ID: []tg.InputMessageClass{&tg.InputMessageID{ID: msgID}},
-		})
-	case *tg.InputPeerUser, *tg.InputPeerChat, *tg.InputPeerSelf:
-		msgClass, err = ctx.Raw.MessagesGetMessages(ctx, []tg.InputMessageClass{
-			&tg.InputMessageID{ID: msgID},
-		})
-	default:
-		return nil, nil, fmt.Errorf("unsupported peer type: %T", inputPeer)
-	}
-
-	if err != nil {
-		log.Printf("failed to get messages: %v", err)
-		return nil, nil, err
-	}
-
-	var messages []tg.MessageClass
-	var users []tg.UserClass
-
-	switch r := msgClass.(type) {
-	case *tg.MessagesChannelMessages:
-		messages = r.Messages
-		users = r.Users
-	case *tg.MessagesMessages:
-		messages = r.Messages
-		users = r.Users
-	case *tg.MessagesMessagesSlice:
-		messages = r.Messages
-		users = r.Users
-	default:
-		return nil, nil, fmt.Errorf("unknown messages response type: %T", msgClass)
-	}
-
-	if len(messages) == 0 {
-		return nil, nil, nil
-	}
-
-	msg, ok := messages[0].(*tg.Message)
-	if !ok {
-		return nil, nil, nil
-	}
-
-	userMap := make(map[int64]*tg.User)
-	for _, u := range users {
-		if user, ok := u.(*tg.User); ok {
-			userMap[user.ID] = user
-		}
-	}
-	return msg, userMap, nil
 }
